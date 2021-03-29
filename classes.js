f1a38af45dd200c4c6db8b7cba60de03a1fd5721
chro20ai <chri20ai@student.cbs.dk>
2021-03-29 13:14:13 +0200

    classes

---
+++ classes.js
@@ -0,0 +1,30 @@
+class User{
+    constructor(ID ,userName, password, firstName, lastName, birthDate, gender, sexualOrientation, address){
+    this._ID = ID
+    this._userName = userName
+    this._password = password
+    this._firstName = firstName 
+    this._lastName = lastName
+    this._birthDate = birthDate
+    this._gender = gender
+    this._sexualOrientation = sexualOrientation
+    this._address = address
+    } 
+}
+
+class Like{
+    constructor(ID, userID, likedID){
+        this._ID = ID
+        this._userID = userID
+        this._likedID = likedID
+}
+}
+
+class Match{
+    constructor(ID, userID1, userID2){
+        this._ID = ID        
+        this._userID1 = userID1
+        this._userID2 = userID2
+    }
+}
+
