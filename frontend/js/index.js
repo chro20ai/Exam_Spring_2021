8290351c728714ad8866db99f00765dcaf0ad2eb
Christoffer Rosing <my@emailaddress.com>
2021-04-08 08:28:50 +0200

    getButton added

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -32,3 +32,25 @@ form.addEventListener("submit", function(e) {
         console.log(err)
     })
 })
+
+var getButton = document.getElementById("checkOut")
+
+getButton.addEventListener('click', function(){
+    var name1 = document.getElementById("name").value
+    fetch(`http://localhost:7071/api/user?name=${name1}`)
+    .then(
+        function(response){
+            if (response.status !== 200){
+                console.log("Something went wrong " + response.status);
+                return 
+            }
+
+            response.json().then(function(data) {
+                console.log(data)
+            });
+        }
+    )
+    .catch(function(err){
+        console.log(err)
+    })
+})
