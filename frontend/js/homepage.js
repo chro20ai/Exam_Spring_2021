c63d6caa35e30ecc61e4e902dec3103ac0224ae1
chro20ai <chri20ai@student.cbs.dk>
2021-04-13 11:12:05 +0200

    delete

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,12 +1,18 @@
 var deleteButton = document.getElementById("delete")
+console.log(deleteButton)
+console.log("hej1")
 
-
-deleteButton.addEventListener("submit", function(e) {
+deleteButton.addEventListener("click", function(e) {
     e.preventDefault()
 
-    var id = //Skal hentes fra localstorage
+    var id = localStorage.getItem("loggedIn")
+    console.log(id)
     fetch(`http://localhost:7071/api/deleteProfile?id=${id}`, {
+        //fetch("http://localhost:7071/api/deleteProfile?id=", + localStorage.getItem("loggedIn") {
         method: 'DELETE',
+        body: JSON.stringify({
+            id: id
+        }),
         headers: {
             "Content-Type": "application/json; charset-UTF-8"
         }
--- shared/db.js
+++ shared/db.js
@@ -106,20 +106,17 @@ module.exports.insertlogin = insertlogin;
 
 
 
-function deleteStatement(payload){
+function deleteStatement(id){
     return new Promise((resolve, reject) => {
     const sql = "DELETE FROM [eksamen].[user] WHERE id = @id"; 
         const request = new Request(sql, (err) => {
             if(err){
                 reject({message: "error connection"})    
-            }});
-
-        request.addParameter('id', TYPES.Int, payload.id)        
-        request.on('row', (row) => {
-            console.log('User inserted', row)
-            resolve('User inserted', row)
-        });
-
+            }}); 
+                request.addParameter('id', TYPES.Int, id)     
+                request.on('row', (columns) => {
+                resolve(columns)
+            });
     connection.execSql(request)
 
 
