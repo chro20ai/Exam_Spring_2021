e564cbeb1e1599e25b6075a9e218dcbe80ff515e
PeterRohmann1 <prs1997@hotmail.com>
2021-04-21 14:21:00 +0200

    igangmedregionsort

--- frontend/js/date.js
+++ frontend/js/date.js
@@ -33,16 +33,18 @@ graph.addEdge("sjaelland", "hovedstaden")
 //Hovedstaden
 graph.addEdge("hovedstaden", "sjaelland")
 
-console.log(graph.ShortestPathBFS("nordjylland", "hovedstaden"))
+console.log(graph.ShortestPathBFS("nordjylland", "midtjylland"))
 
 
 
-//lav om til 3140 osv.
-//lav agerange i localstorage
-var array1825 = []
-var array2630 = []
-var array3140 = []
-var array41 = []
+var array = []
+var regionarray = []
+var sjaellandarray = []
+var syddanmarkarray = []
+var nordjyllandarray = []
+var midtjyllandarray = []
+var hovedstadenarray = []
+var totalarray = []
 
 function getAge(dateString) 
 {
@@ -70,24 +72,31 @@ function getAge(dateString)
                     for( i = 0; i < data.length; i ++){
                         var age = getAge(data[i][5].value)
                         //console.log(age)
-                        if(age >= 18 && age <= 25){
-                        array1825.push(data[i])
+                        if(age >= 18 && age <= 25 && agerange == "18-25"){
+                        array.push(data[i])
                         }
-                        else if(age >= 26 && age <= 30){
-                        array2630.push(data[i])
+                        else if(age >= 26 && age <= 30 && agerange == "26-30"){
+                        array.push(data[i])
                         }
-                        else if(age >= 31 && age <= 40){
-                        array3140.push(data[i])
+                        else if(age >= 31 && age <= 40 && agerange == "31-40"){
+                        array.push(data[i])
                         }    
-                        else if(age >= 41){
-                        array41.push(data[i])
+                        else if(age >= 41 && agerange == "41+"){
+                        array.push(data[i])
                         }
                     }
-                    console.log(array1825)
-                    console.log(array2630)
-                    console.log(array3140)
-                    console.log(array41)
-                    //console.log(data)
+                    console.log(array)
+                    
+                    
+                    for( i = 0; i < array.length; i ++){
+                        
+                            regionarray.push(graph.ShortestPathBFS(region, array[i][9].value))
+                            totalarray.push(regionarray)
+                        
+                        
+                    }
+                    console.log(totalarray)
+                    console.log(regionarray)
                 })
             }
         )
@@ -107,47 +116,17 @@ var age;
 var gender;
 
 
+
+
 try {
-    if(agerange == "18-25"){
-        if(array1825[swipeindex][0].value == id){
+    if(array[swipeindex][0].value == id){
             swipeindex++;
             }
-            username = array1825[swipeindex][1].value
-            firstname = array1825[swipeindex][3].value
-            lastname = array1825[swipeindex][4].value
-            age = getAge(array1825[swipeindex][5].value)
-            gender = array1825[swipeindex][6].value    
-    }
-    else if(agerange == "26-30"){
-        if(array2630[swipeindex][0].value == id){
-        swipeindex++;
-        }
-        username = array2630[swipeindex][1].value
-        firstname = array2630[swipeindex][3].value
-        lastname = array2630[swipeindex][4].value
-        age = getAge(array2630[swipeindex][5].value)
-        gender = array2630[swipeindex][6].value
-    }
-    else if(agerange == "31-40"){
-        if(array3140[swipeindex][0].value == id){
-            swipeindex++;
-            }
-        username = array3140[swipeindex][1].value
-        firstname = array3140[swipeindex][3].value
-        lastname = array3140[swipeindex][4].value
-        age = getAge(array3140[swipeindex][5].value)
-        gender = array3140[swipeindex][6].value
-    }
-    else if(agerange == "41+"){
-        if(array41[swipeindex][0].value == id){
-            swipeindex++;
-            }
-        username = array41[swipeindex][1].value
-        firstname = array41[swipeindex][3].value
-        lastname = array41[swipeindex][4].value
-        age = getAge(array41[swipeindex][5].value)
-        gender = array41[swipeindex][6].value
-    }
+            username = array[swipeindex][1].value
+            firstname = array[swipeindex][3].value
+            lastname = array[swipeindex][4].value
+            age = getAge(array[swipeindex][5].value)
+            gender = array[swipeindex][6].value    
   }
   catch(err) {
     alert("No more users for you, checkout out pornhub")
